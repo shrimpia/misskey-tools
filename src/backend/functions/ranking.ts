c717a42bb6f312805b4fe49e0b446f06ab4e7b34
Xeltica <xeltica@gmail.com>
2021-10-18 03:09:41 +0900

    ランキング10位以上が表示されない不具合を修正

--- src/backend/functions/ranking.ts
+++ src/backend/functions/ranking.ts
@@ -6,12 +6,12 @@ import { User } from '../models/entities/user';
  * @param limit 取得する件数
  * @returns ミス廃ランキング
  */
-export const getRanking = async (limit: number | null = 10): Promise<User[]> => {
+export const getRanking = async (limit?: number | null): Promise<User[]> => {
 	const query = Users.createQueryBuilder('user')
 		.where('"user"."bannedFromRanking" IS NOT TRUE')
 		.orderBy('"user".rating', 'DESC');
 
-	if (limit !== null) {
+	if (limit) {
 		query.limit(limit);
 	}
 
