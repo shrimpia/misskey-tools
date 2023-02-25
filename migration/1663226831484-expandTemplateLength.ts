bfc725f46802c172ffaf0a9a2245807cbb91a166
Xeltica <xeltica@gmail.com>
2023-02-25 17:16:34 +0900

    lintがうるさいんじゃ

--- migration/1663226831484-expandTemplateLength.ts
+++ migration/1663226831484-expandTemplateLength.ts
@@ -1,13 +1,13 @@
 import {MigrationInterface, QueryRunner} from 'typeorm';
 
 export class expandTemplateLength1663226831484 implements MigrationInterface {
-    name = 'expandTemplateLength1663226831484'
+  name = 'expandTemplateLength1663226831484';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    	await queryRunner.query('ALTER TABLE "user" ALTER COLUMN "template" TYPE character varying(1024)');
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('ALTER TABLE "user" ALTER COLUMN "template" TYPE character varying(1024)');
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    	await queryRunner.query('ALTER TABLE "user" ALTER COLUMN "template" TYPE character varying(280)');
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('ALTER TABLE "user" ALTER COLUMN "template" TYPE character varying(280)');
+  }
 }
