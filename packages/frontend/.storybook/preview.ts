0461fa470ee91812768957b1bb96cdf1f72c3bfa
Xeltica <xeltica@gmail.com>
2023-04-30 02:40:46 +0900

    fix(frontend): storybookが動かないのを修正

--- packages/frontend/.storybook/preview.ts
+++ packages/frontend/.storybook/preview.ts
@@ -1,7 +1,7 @@
 import type { Preview } from "@storybook/react";
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
-import {darkTheme, globalStyles, theme} from '../libs/stitches';
+import {darkTheme, globalStyles, theme} from '../src/libs/stitches';
 
 import 'ress';
 import '@tabler/icons-webfont/tabler-icons.min.css';
