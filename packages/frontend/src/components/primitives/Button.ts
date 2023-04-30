6c85bcd9c79df9956f10ca958373aa228605090f
Xeltica <xeltica@gmail.com>
2023-04-30 13:50:51 +0900

    chore(frontend): ボタンの調整

--- packages/frontend/src/components/primitives/Button.ts
+++ packages/frontend/src/components/primitives/Button.ts
@@ -14,6 +14,7 @@ export const Button = styled('button', {
   justifyContent: 'center',
   appearance: 'none',
   cursor: 'pointer',
+  lineHeight: 1.5,
 
   '&:disabled': {
     cursor: 'not-allowed',
